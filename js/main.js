de3066fde9a7e332d5db67182a2b116dd582b48a
Khaled Radwan <khaledradwan96@gmail.com>
2024-06-28 23:55:25 +0300

    code function getWeather()

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-const key = '223aedbd1377460cafe02500242806'
+const apiKey = '223aedbd1377460cafe02500242806'
+const api = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=london&days=3`
+console.log(api);
 
- const api = `http://api.weatherapi.com/v1/forecast.xml?key=${key}&q=07112&days=7`
 
- console.log(api);
+async function getWeather(){
+    let response = await fetch(api)
+    let myData = await response.json()
+
+    console.log(myData)
+    console.log(myData.forecast)
+    console.log(myData.forecast.forecastday)
+    console.log(myData.forecast.forecastday[0].date)
+
+   return myData
+}
+
+

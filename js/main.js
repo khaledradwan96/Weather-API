902407f7978940c68320cf2d01c0807ee027d4c2
Khaled Radwan <khaledradwan96@gmail.com>
2024-06-30 20:03:50 +0300

    add code of search and fix error in function displayTodayData(data)

--- js/main.js
+++ js/main.js
@@ -1,27 +1,49 @@
-const apiKey = '223aedbd1377460cafe02500242806'
-const api = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=london&days=3`
-// console.log(api);
-
-
-async function getWeather(){
+async function getWeather(cityName){
+    const apiKey = '223aedbd1377460cafe02500242806'
+    const api = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=3`
+    console.log(api);
     let response = await fetch(api)
     let myData = await response.json()
-   return myData
+    return myData
 }
 
 
 function displayTodayData(data){
     // console.log(data);
 
-    document.getElementById("city").innerHTML = data.location.name
-    document.getElementById("country").innerHTML = data.location.country
-    document.getElementById("degree").innerHTML = data.current.temp_c + `<sup>o</sup>C`
-    document.getElementById("statusImg").setAttribute('src', data.current.condition.icon)
-    document.getElementById("statusText").innerHTML = data.current.condition.text
-
-    document.getElementById("humidity").innerHTML = data.current.humidity + '%'
-    document.getElementById("wind_kph").innerHTML = data.current.wind_kph + 'km/h'
-    document.getElementById("wind_dir").innerHTML = data.current.wind_dir
+    let TodayBox = `
+        <!-- Today Date -->
+        <div class="col-lg-4">
+            <div class="date d-flex justify-content-between">
+                <span class="dayName"></span>
+                <span id="dayDate"></span>
+            </div>
+            <div class="weather">
+                <div class="location">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <span id="city">${data.location.name}</span> -
+                    <span id="country">${data.location.country}</span>
+                </div>
+                <div id="degree">${data.current.temp_c}<sup>o</sup>C</div>
+                <img id="statusImg" src="${data.current.condition.icon}">
+                <span id="statusText">${data.current.condition.text}</span>
+                <div class="info">
+                    <div>
+                        <img src="assets/asset 2.png">
+                        <span id="humidity">${data.current.humidity} %</span>
+                    </div>
+                    <div>
+                        <img src="assets/asset 3.png">
+                        <span id="wind_kph">${data.current.wind_kph} km/h</span>
+                    </div>
+                    <div>
+                        <img src="assets/asset 4.png">
+                        <span id="wind_dir">${data.current.wind_dir}</span>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    document.querySelector('.weather-container .row').innerHTML = TodayBox
 }
 
 function displayNextData(data){
@@ -31,12 +53,11 @@ function displayNextData(data){
     let cartona = ""
     for(let i=0; i<forcastData.length - 1; i++){
         cartona += 
-            `<div class="col-lg-4 text-center">
-                <div class="date">
-                    <span class="dayName"></span>
-                </div>
+            `<!-- Next ${i+1} day Date -->
+            <div class="col-lg-4 text-center">
+                <div class="date dayName"></div>
                 <div class="weather">
-                <img class="statusImg" src="${forcastData[i+1].day.condition.icon}">
+                    <img class="statusImg" src="${forcastData[i+1].day.condition.icon}">
                     <div class="fs-1 fw-bold">${forcastData[i+1].day.maxtemp_c}<sup>o</sup>C</div>
                     <div class="text-secondary">${forcastData[i+1].day.mintemp_c}<sup>o</sup>C</div>
                     <span class="status">${forcastData[i+1].day.condition.text}</span>
@@ -50,11 +71,14 @@ function displayDate(data){
     let date = data.forecast.forecastday
     // console.log(date)
 
+    // for Display Day Name
     for(let i=0; i<date.length;i++){
         let dayName = new Date(date[i].date).toLocaleDateString('en-us',{weekday: "long"})
         // console.log(dayName);
         document.querySelectorAll(".dayName")[i].innerHTML = dayName
     }
+
+    // to Display Day Date
     let dayNum = new Date(date[0].date).getDate()
     let dayMonth = new Date(date[0].date).toLocaleDateString('en-us',{month: "long"})
     let dayDate = dayNum + " " + dayMonth
@@ -64,10 +88,24 @@ function displayDate(data){
 
 
 // start app
-async function startApp(){
-    let weatherData = await getWeather()
-    displayTodayData(weatherData)
-    displayNextData(weatherData)
-    displayDate(weatherData)
+async function startApp(cityName = "cairo"){
+    let weatherData = await getWeather(cityName)
+    if(!weatherData.error){
+        displayTodayData(weatherData)
+        displayNextData(weatherData)
+        displayDate(weatherData)
+    }
 }
 startApp()
+
+
+let searchInput = document.getElementById("searchInput")
+searchInput.addEventListener('input', function(){
+    // console.log(searchInput.value)
+    startApp(searchInput.value)
+})
+
+let searchBtn = document.getElementById("searchBtn")
+searchBtn.addEventListener('click', function(e){
+    e.preventDefault()
+})

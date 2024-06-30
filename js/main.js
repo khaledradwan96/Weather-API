795ab659fa89ec9294738b9b08f9e643a71fcd8c
Khaled Radwan <khaledradwan96@gmail.com>
2024-06-30 15:44:53 +0300

    add code of function displayNextData(data)

--- js/main.js
+++ js/main.js
@@ -11,8 +11,7 @@ async function getWeather(){
 
 
 function displayTodayData(data){
-    console.log(data);
-    console.log(data.current);
+    // console.log(data.current);
 
     document.getElementById("city").innerHTML = data.location.name
     document.getElementById("country").innerHTML = data.location.country
@@ -25,10 +24,33 @@ function displayTodayData(data){
     document.getElementById("wind_dir").innerHTML = data.current.wind_dir
 }
 
+function displayNextData(data){
+    let forcastData = data.forecast.forecastday
+    console.log(forcastData)
+
+    let cartona = ""
+    for(let i=0; i<forcastData.length - 1; i++){
+        cartona += 
+            `<div class="col-lg-4 text-center">
+                <div class="date d-flex justify-content-between">
+                    <span>Friday</span>
+                    <span>28 june</span>
+                </div>
+                <div class="weather">
+                <img class="statusImg" src="${forcastData[i+1].day.condition.icon}">
+                    <div class="fs-1 fw-bold">${forcastData[i+1].day.maxtemp_c}<sup>o</sup>C</div>
+                    <div class="text-secondary">${forcastData[i+1].day.mintemp_c}<sup>o</sup>C</div>
+                    <span class="status">${forcastData[i+1].day.condition.text}</span>
+                </div>
+            </div>`
+    }
+    document.querySelector('.weather-container .row').innerHTML += cartona
+}
 
 // start app
 async function startApp(){
     let weatherData = await getWeather()
     displayTodayData(weatherData)
+    displayNextData(weatherData)
 }
 startApp()

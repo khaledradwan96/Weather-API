9e00a103b1b2fc7e458a98f132f0c3664802de18
Khaled Radwan <khaledradwan96@gmail.com>
2024-06-30 15:03:13 +0300

    add code of function displayTodayData(data)

--- js/main.js
+++ js/main.js
@@ -6,13 +6,29 @@ console.log(api);
 async function getWeather(){
     let response = await fetch(api)
     let myData = await response.json()
-
-    console.log(myData)
-    console.log(myData.forecast)
-    console.log(myData.forecast.forecastday)
-    console.log(myData.forecast.forecastday[0].date)
-
    return myData
 }
 
 
+function displayTodayData(data){
+    console.log(data);
+    console.log(data.current);
+
+    document.getElementById("city").innerHTML = data.location.name
+    document.getElementById("country").innerHTML = data.location.country
+    document.getElementById("degree").innerHTML = data.current.temp_c + `<sup>o</sup>C`
+    document.getElementById("statusImg").setAttribute('src', data.current.condition.icon)
+    document.getElementById("statusText").innerHTML = data.current.condition.text
+
+    document.getElementById("humidity").innerHTML = data.current.humidity + '%'
+    document.getElementById("wind_kph").innerHTML = data.current.wind_kph + 'km/h'
+    document.getElementById("wind_dir").innerHTML = data.current.wind_dir
+}
+
+
+// start app
+async function startApp(){
+    let weatherData = await getWeather()
+    displayTodayData(weatherData)
+}
+startApp()
